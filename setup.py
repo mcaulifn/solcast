42cc3fd771efb690e6bb91f7b8475a63af6ff314
Nathaniel McAuliffe <mcaulifn@outlook.com>
2020-01-27 14:53:48 -0500

    maybe this version parameter

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
 setup_requirements = ['pytest-runner', ]
 
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mcaulifn/solcast',
-    version='0.1.0',
+    version='0.5.0',
     zip_safe=False,
 )
