2c322e903cfab7510f2204a5c8d7cdef6c783d8a
Nathaniel McAuliffe <mcaulifn@outlook.com>
2020-01-27 14:30:23 -0500

    pylint

--- solcast/world.py
+++ solcast/world.py
@@ -29,7 +29,8 @@ class World(Solcast):
         :param longitude: The longitude of the location (EPSG:4326)
         :param hours: Time window of the response in hours
         :return: forecasts
-        :raises ValidationError: Latitude, longitude or hours are invalid, see response_status for further details
+        :raises ValidationError:
+            Latitude, longitude or hours are invalid, see response_status for further details
         :raises SiteNotFound: The location is outside our coverage area.
         """
         endpoint = 'estimated_actuals'
