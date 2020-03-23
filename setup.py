6f93b2241f05801a584c65520a5b3b79d8fef646
Nathaniel McAuliffe <mcaulifn@outlook.com>
2020-03-23 09:31:32 -0400

    fixing version and requirements

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Nathaniel McAuliffe",
     author_email='nathanielmcauliffe@hotmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
