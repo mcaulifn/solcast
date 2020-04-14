dda659efd351de2c1a3013810b3461e13b6143a3
Nathaniel McAuliffe <nathanielmcauliffe@hotmail.com>
2020-04-14 14:06:59 -0400

    using setuptools_scm

--- pysolcast/__init__.py
+++ pysolcast/__init__.py
@@ -2,4 +2,3 @@
 
 __author__ = """Nathaniel McAuliffe"""
 __email__ = 'nathanielmcauliffe@hotmail.com'
-__version__ = '1.0.0'
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 """The setup script."""
 
 from setuptools import setup, find_packages
+import setuptools_scm
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -12,9 +13,15 @@ with open('HISTORY.rst') as history_file:
 
 requirements = []
 
-setup_requirements = ['pytest-runner', ]
-
 test_requirements = ['pytest>=3', ]
+
+
+def local_scheme(version):
+    print(version)
+    if version.branch == 'master' and not version.dirty:
+        return ''
+    return setuptools_scm.version.get_local_node_and_date(version)
+
 
 setup(
     author="Nathaniel McAuliffe",
@@ -35,10 +42,15 @@ setup(
     keywords='solcast',
     name='pysolcast',
     packages=find_packages(include=['pysolcast', 'pysolcast.*']),
-    setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mcaulifn/solcast',
+    use_scm_version={
+        'local_scheme': local_scheme,
+        'write_to': '',
+        'write_to_template': '__version__ = "{version}"\n',
+        'tag_regex': r'^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$'
+    },
     version='1.0.0',
     zip_safe=False,
 )
