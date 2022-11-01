abd627ec5baabc10cc1931e3a6315fbc027df51f
Nathaniel McAuliffe <nathanielmcauliffe@hotmail.com>
2022-11-01 10:02:50 -0400

    build updates

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Nathaniel McAuliffe",
     author_email='nathanielmcauliffe@hotmail.com',
-    python_requires='>=3.7',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     description="Solcast API",
     install_requires=requirements,
