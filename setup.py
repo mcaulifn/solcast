0721e31a610cb002cd33d4cd149c915400f88ffa
Nathaniel McAuliffe <nathanielmcauliffe@hotmail.com>
2023-01-08 09:20:40 -0500

    fix(python): Add support for 3.8

--- setup.py
+++ setup.py
@@ -18,12 +18,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Nathaniel McAuliffe",
     author_email='nathanielmcauliffe@hotmail.com',
-    python_requires='>=3.9',
+    python_requires='>=3.8',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
